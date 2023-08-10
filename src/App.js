c5d9a3857d8bb9131ac23c4b234cc0e9b1fbd419
Erdem Yılmaz <62537146+erdemylmaz@users.noreply.github.com>
2023-08-10 13:43:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -896,7 +896,7 @@ function updateRanking() {
   get(ref(db, "APP/USERS")).then((snapshot) => {
     users = snapshot.val();
 
-    if(localStorage.getItem('operationName') {
+    if(localStorage.getItem('operationName')) {
       let user = null;
   
       operationName = localStorage.getItem('operationName');
